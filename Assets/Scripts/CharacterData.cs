ee1d6b561e387151f3049719f822b22469501bc1
Kim donguk <baby00du20@gmail.com>
2026-08-10 18:14:51 +0900

    캐릭터데이터 추가, 스킬데이터에 가중치 관련 내용 추가

---
+++ Assets/Scripts/CharacterData.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Limbus.Data
+{
+    [Serializable] public class AttackResistances
+    {
+        [SerializeField] private float _slash = 1.0f;  // 내성 계수 : 0.5 = 내성, 2.0 = 취약)
+        [SerializeField] private float _pierce = 1.0f;
+        [SerializeField] private float _blunt = 1.0f;
+
+        public float Slash => _slash;
+        public float Pierce => _pierce;
+        public float Blunt => _blunt;
+    }
+
+    [Serializable]
+    public class SanityConditionData
+    {
+        [SerializeField] private int _maxSanity = 45;
+        [SerializeField] private int _minSanity = -45;
+
+        [Header("정신력 증가 조건 수치")]
+        [SerializeField] private int _clashWinSpGain = 10;
+        [SerializeField] private int _killEnemySpGain = 10;
+        [SerializeField] private int _allyClashWinSpGain = 5;
+
+        [Header("정신력 감소 조건 수치")]
+        [SerializeField] private int _clashLoseSpLoss = 10;
+        [SerializeField] private int _allyDeathSpLoss = 10;
+
+        public int MaxSanity => _maxSanity;
+        public int MinSanity => _minSanity;
+        public int ClashWinSpGain => _clashWinSpGain;
+        public int KillEnemySpGain => _killEnemySpGain;
+        public int AllyDeathSpLoss => _allyDeathSpLoss;
+    }
+
+    [CreateAssetMenu(fileName = "NewCharacterData", menuName = "Limbus/Data/Character Data")]
+    public class CharacterData : ScriptableObject
+    {
+        [Header("수감자 및 인격 기본 정보")]
+        [Tooltip("수감자 ID")]
+        [SerializeField] private string _characterId;
+
+        [Tooltip("수감자 이름")]
+        [SerializeField] private string _characterName;
+
+        [Tooltip("인격 ID")]
+        [SerializeField] private string _identityId;
+
+        [Tooltip("인격 이름")]
+        [SerializeField] private string _identityName;
+
+        [SerializeField] private Sprite _characterPortrait;
+
+        [Header("소속 및 특성 키워드")]
+        [SerializeField] private List<string> _factionKeywords = new List<string>();
+
+        [Header("기본 스탯 및 레벨별 성장 계수")]
+        [Tooltip("현재 인격 레벨 상한")]
+        [SerializeField] private int _maxLevel = 60;
+
+        [SerializeField] private int _baseMaxHp = 100;
+        [SerializeField] private float _hpGrowthRate = 10.0f;
+
+        [SerializeField] private int _baseOffenseLevel = 10;
+        [SerializeField] private float _offenseGrowthRate = 1.0f;
+
+        [SerializeField] private int _baseDefenseLevel = 10;
+        [SerializeField] private float _defenseGrowthRate = 1.0f;
+
+        [Header("속도 범위")]
+        [SerializeField] private int _minSpeed = 2;
+        [SerializeField] private int _maxSpeed = 5;
+
+        [Header("내성 정보 및 정신력 메커니즘")]
+        [SerializeField] private AttackResistances _attackResistances;
+        [SerializeField] private SanityConditionData _sanityCondition;
+
+        [Header("보유 스킬 정보")]
+        [SerializeField] private List<SkillData> _skillList = new List<SkillData>();
+        [SerializeField] private SkillData _defenseSkill;
+
+
+        public string CharacterId => _characterId;
+        public string CharacterName => _characterName;
+        public string IdentityId => _identityId;
+        public string IdentityName => _identityName;
+        public Sprite CharacterPortrait => _characterPortrait;
+
+        public IReadOnlyList<string> FactionKeywords => _factionKeywords;
+
+        public int MinSpeed => _minSpeed;
+        public int MaxSpeed => _maxSpeed;
+
+        public AttackResistances AttackResistances => _attackResistances;
+        public SanityConditionData SanityCondition => _sanityCondition;
+
+        public IReadOnlyList<SkillData> SkillList => _skillList;
+        public SkillData DefenseSkill => _defenseSkill;
+
+
+        public int GetMaxHp(int level) => Mathf.RoundToInt(_baseMaxHp + (Mathf.Min(level, _maxLevel) - 1) * _hpGrowthRate);
+        public int GetOffenseLevel(int level) => Mathf.RoundToInt(_baseOffenseLevel + (Mathf.Min(level, _maxLevel) - 1) * _offenseGrowthRate);
+        public int GetDefenseLevel(int level) => Mathf.RoundToInt(_baseDefenseLevel + (Mathf.Min(level, _maxLevel) - 1) * _defenseGrowthRate);
+    }
+}
--- Assets/Scripts/SkillData.cs
+++ Assets/Scripts/SkillData.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 namespace Limbus.Data
 {
-    [Serializable] public class ResourceCost
+    [Serializable]
+    public class ResourceCost
     {
         [SerializeField] private ResourceConsumeTrigger _resourceTrigger = ResourceConsumeTrigger.OnUse;
         [SerializeField] private int _hpCost;
@@ -33,17 +34,29 @@ namespace Limbus.Data
         [SerializeField] private AttackType _attackType = AttackType.None;
         [SerializeField] private DefenseType _defenseType = DefenseType.None;
 
+        [Header("타겟팅 및 가중치 메커니즘")]
+        [Tooltip("공격 가중치")]
+        [Range(1, 7)]
+        [SerializeField] private int _targetWeight = 1;
+
+        [Tooltip("공격 대상")]
+        [SerializeField] private TargetType _targetType = TargetType.Enemy;
+
+        [Tooltip("광역 난사 여부s")]
+        [SerializeField] private bool _isRandomMultiTarget = false;
+
         [Header("위력 및 코스트 매커니즘")]
         [SerializeField] private int _basePower;
         [SerializeField] private ResourceCost _resourceCost;
 
-        [Tooltip("스킬 사용 시 획득하는 E.G.O 자원 매커니즘")]
+        [Tooltip("스킬 사용 시 획득하는 E.G.O 자원 수량")]
         [SerializeField] private int _egoResourceGainAmount = 1;
 
         [SerializeField] private bool _isAssistSkill;
 
         [Header("코인 데이터")]
-        [SerializeField] private List<CoinData> _coins = new List<CoinData>();
+        [SerializeField] private List<CoinData> _coins = new();
+
 
         public string SkillId => _skillId;
         public string SkillName => _skillName;
@@ -54,10 +67,15 @@ namespace Limbus.Data
         public SinAttribute SinAttribute => _sinAttribute;
         public AttackType AttackType => _attackType;
         public DefenseType DefenseType => _defenseType;
+
+        public int TargetWeight => _targetWeight;
+        public TargetType TargetType => _targetType;
+        public bool IsRandomMultiTarget => _isRandomMultiTarget;
+
         public int BasePower => _basePower;
         public ResourceCost ResourceCost => _resourceCost;
         public int EgoResourceGainAmount => _egoResourceGainAmount;
         public bool IsAssistSkill => _isAssistSkill;
-        public  IReadOnlyList<CoinData> Coins => _coins;
+        public IReadOnlyList<CoinData> Coins => _coins;
     }
 }
